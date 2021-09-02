b88d7aeabdfd9ec140f8843312403e7ee0731468
Ashish Vishal <ashishvishal7810@gmail.com>
2021-09-02 13:56:30 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
-
+import { DISHES } from './shared/dishes';
 
 // function App() {
 //   return (
@@ -19,6 +19,14 @@ import './App.css';
 // }
 
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state={
+      dishes:DISHES
+    };
+  }
+
   render() {
     return (
       <div>
@@ -27,7 +35,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
