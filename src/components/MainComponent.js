edbc443b660e61185c06d0ab9127ea761d741cc3
Ashish Vishal <ashishvishal7810@gmail.com>
2021-10-13 09:36:01 +0530

    assignment -week2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -46,6 +47,7 @@ class Main extends Component {
         <Header />
         <Switch>
             <Route path='/home' component = { HomePage } />
+            <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
