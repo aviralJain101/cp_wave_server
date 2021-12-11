4adc49c812a8fde308b6b30ce5e20cddc7aa41b7
Ashish Vishal <ashishvishal7810@gmail.com>
2021-12-11 08:22:58 +0530

    Infinit scrolling continued

--- src/components/AddUsers/AddUserComponent.js
+++ src/components/AddUsers/AddUserComponent.js
@@ -54,9 +54,7 @@ function Renderusers({isLoading, errMess, searchResult, searchTerm}) {
     else if(searchResult){
         const users = searchResult.map((user) => {
             return (
-                    <div className="col-12 mt-2">
-                        <RenderUser user={user} />
-                    </div>
+                <RenderUser user={user} />
             );
         });
 
@@ -79,121 +77,101 @@ class AddUsers extends Component {
             page: 0,
             prevY: 0,
             hasMore: true
-          };
-       }
-
-       componentDidMount() {
-        this.getPhotos(this.state.page);
-    
-        var options = {
-          root: null,
-          rootMargin: "0px",
-          threshold: 1.0
         };
-        
-        this.observer = new IntersectionObserver(
-          this.handleObserver.bind(this),
-          options
-        );
-        this.observer.observe(this.loadingRef);
-      }
+    }
 
-       getPhotos(page) {
-        this.setState({ loading: true });
+    componentDidMount() {
+    this.getUsers(this.state.page);
 
-        const bearer = 'Bearer ' + localStorage.getItem('token');
-        fetch(baseUrl+'search?searchTerm='+this.props.searches.searchTerm+'&page='+page,{
-            headers: {
-                'Authorization': bearer
-            },
-        })
-        .then(response => response.json())
-        .then(searchResult => {
-            this.setState({ users: [...this.state.users, ...searchResult] });
-            console.log(searchResult);
-            this.setState({ loading: false });
-            if(searchResult.length < 6){
-                this.setState({hasMore: false});
-            }
-            console.log(this.state.hasMore)
-            // alert(searchResult.length);
-        })
-        
-        
-      }
+    var options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0
+    };
+    
+    this.observer = new IntersectionObserver(
+        this.handleObserver.bind(this),
+        options
+    );
+    this.observer.observe(this.loadingRef);
+    }
+
+    getUsers(page) {
+    this.setState({ loading: true });
+
+    const bearer = 'Bearer ' + localStorage.getItem('token');
+    fetch(baseUrl+'search?searchTerm='+this.props.searches.searchTerm+'&page='+page,{
+        headers: {
+            'Authorization': bearer
+        },
+    })
+    .then(response => response.json())
+    .then(searchResult => {
+        this.setState({ users: [...this.state.users, ...searchResult] });
+        console.log(searchResult);
+        this.setState({ loading: false });
+        if(searchResult.length < 6){
+            this.setState({hasMore: false});
+        }
+    })
+    
+    
+    }
 
      
 
       handleObserver(entities, observer) {
         const y = entities[0].boundingClientRect.y;
         if (this.state.prevY > y && this.state.hasMore) {
-        //   const curPage = this.state.page+1;
           this.setState({ page: this.state.page+1 });
-          this.getPhotos(this.state.page);
+          this.getUsers(this.state.page);
           
         }
         this.setState({ prevY: y });
       }
     
 
-      render() {
+    render() {
 
         // Additional css
         const loadingCSS = {
-          height: "400px",
-          margin: "30px"
+            height: "400px",
+            margin: "30px"
         };
-    
+        
         // To change the loading icon behavior
-    const loadingTextCSS = { display: this.state.loading ? "block" : "none" };
+        const loadingTextCSS = { display: this.state.loading ? "block" : "none" };
 
-    return (
-        <div className="container">
-        <div>
-            {this.state.users.map(user => (
-                <RenderUser user={user} />
-            ))}
-        </div>
-        <div>
-            {!this.state.hasMore ?
-                <h4>Yay!! you have visited it all</h4>:
-                null
-            }
-        </div>
-            
-        <div
-            ref={loadingRef => (this.loadingRef = loadingRef)}
-            style={loadingCSS}
-        >
-            <span style={loadingTextCSS}>Loading...</span>
-        </div>
-        </div>
-    );
-    }
-
-    // render() {
-    //     // alert(this.props.searches.isLoading);
-    //     return (
-    //         <div className="container">
-    //             <div className="row">
-    //                 <Breadcrumb>
-    //                     <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
-    //                     <BreadcrumbItem active>AddUsers</BreadcrumbItem>
-    //                 </Breadcrumb>
-    //                 <div className="col-12">
-                        
-    //                     <Renderusers 
-    //                     isLoading={this.props.searches.isLoading}
-    //                     errMess={this.props.searches.errMess}
-    //                     searchResult={this.props.searches.searchResult}
-    //                     searchTerm={this.props.searches.searchTerm} />
-    //                 </div>
-    //             </div>
+        return (
+            <div className="container">
+                <div>
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>AddUsers</BreadcrumbItem>
+            </Breadcrumb>
+            </div>
+            <div>
                 
-    //         </div>
-    //     );
-
-    // }
+                    {this.state.users.map(user => (
+                        <RenderUser user={user} />
+                    ))}
+                </div>
+                <div>
+                    {!this.state.hasMore ?
+                        <h4>Yay!! you have visited it all</h4>:
+                        null
+                    }
+                </div>
+                    
+                <div
+                    ref={loadingRef => (this.loadingRef = loadingRef)}
+                    style={loadingCSS}
+                >
+                    <span style={loadingTextCSS}>Loading...</span>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default AddUsers;
