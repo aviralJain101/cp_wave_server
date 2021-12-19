42ac68f8708b83e1bce9d80eb1b7909fc04cdee6
Ashish Vishal <ashishvishal7810@gmail.com>
2021-12-20 01:22:01 +0530

    olx clone completed

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,4 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { createForms } from 'react-redux-form';
-
 import { Auth } from './auth';
 import { Suggestions } from './suggestions';
 import { SellItem } from './Sell/SellItem';
@@ -8,21 +6,15 @@ import { MarketItem } from './Market/MarketItem';
 import { PurchasedItem } from './Purchased/PurchasedItem';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { InitialFeedback } from './forms';
-import { Searches } from './searches';
 
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
             auth: Auth,
             suggestions: Suggestions,
-            searches: Searches,
             sellItem: SellItem,
             marketItem: MarketItem,
-            purchasedItem: PurchasedItem,
-            ...createForms({
-                feedback: InitialFeedback
-            })
+            purchasedItem: PurchasedItem
         }),
         applyMiddleware(thunk, logger)
     );
